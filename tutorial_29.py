6517f2b4cab2acf0783e64cdeeeef06981c77c69
Prasadkk <143005950+prasad1057@users.noreply.github.com>
2025-02-04 00:53:25 +0530

    DocStrings   &   PEP-8

--- tutorial_29.py
+++ tutorial_29.py
@@ -1,6 +1,7 @@
 # /// DocStrings   &   PEP-8///
 #  docstring is written just after the function ......
 
+
 def square(n):
     '''Takes in a number n, returns the square of n'''       # this is the docstring & it is differnet from comments
     print(n ** 2)
